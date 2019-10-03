837273329e4b7c82dc9ef3c5d7ddc9635c90f97a
satvasu <sathishkumar64@gmail.com>
2019-10-03 17:35:14 +0530

    updated code

---
+++ streamexp/client/client.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/sathishkumar64/grpc_pack/streamexp"
+	"google.golang.org/grpc"
+	"io"
+	"log"
+	"os"
+)
+
+func main()  {
+	con, err := grpc.Dial(":8888",grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Server is not reachable :%v", err)
+	}
+	client :=streamexp.NewReadMyStoryServiceClient(con)
+//	err = getStory(context.Background(), client)
+/*	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}*/
+
+	//err = ListOfStory(context.Background(), client)
+	/*if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}*/
+
+	err = StoryStatus(context.Background(), client)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+}
+
+func getStory(ctx context.Context, client streamexp.ReadMyStoryServiceClient) error {
+	vo, err := client.GetStory(ctx, &streamexp.RequestReadStory{Course: "GO"})
+	if err != nil {
+		log.Fatalf("%v.GetFeatures(_) = _, %v: ", client, err)
+	}
+	log.Println(vo)
+	return err
+}
+
+func ListOfStory(ctx context.Context, client streamexp.ReadMyStoryServiceClient) error {
+	stream, err := client.ListOfStory(ctx, &streamexp.RequestReadStory{Course: "GO"})
+	if err != nil {
+		log.Fatalf("%v.GetFeatures(_) = _, %v: ", client, err)
+	}
+	for {
+		course, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v.ListOfStory(_) = _, %v", client, err)
+		}
+		log.Println(course)
+	}
+	return err
+}
+
+func StoryStatus(ctx context.Context ,client streamexp.ReadMyStoryServiceClient) error{
+	stream, err := client.StoryStatus(ctx)
+	courseList := []string {"GO","Java","HTML","Mule"}
+	for _, crs := range courseList {
+		if err := stream.Send(&streamexp.RequestReadStory{Course: crs}); err != nil {
+			log.Fatalf("%v.Send(%v) = %v", stream, crs, err)
+		}
+	}
+	reply, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("%v.CloseAndRecv() got error %v, want %v", stream, err, nil)
+	}
+	log.Printf("Story Status: %v", reply)
+	return err
+}
--- streamexp/server/server.go
+++ streamexp/server/server.go
@@ -2,45 +2,172 @@ package main
 
 import (
 	"context"
-	"log"
-	"net"
-
+	"encoding/json"
 	"github.com/sathishkumar64/grpc_pack/streamexp"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"io"
+	"log"
+	"net"
 )
 
 // ReadMyStoryServiceServer as interface
-type ReadMyStoryServiceServer struct {
+type ReadServer struct {
 }
+func generateStub() []byte{
+	var exampleData = []byte(`[{  
+        "Course": "HTML",
+        "TutorName": "BABU",
+ 		"Status": true
+    },
+ 	{  
+         "Course": "JAVA",
+        "TutorName": "BALU",
+ 		"Status": true
+    },
+	{
+         "Course": "MongoDB",
+        "TutorName": "Kumar",
+ 		"Status": true
+    },
+	{
+        "Course": "GO",
+        "TutorName": "Sathish",
+ 		"Status": true
+    },
+	{
+		"Course": "Graphql",
+        "TutorName": "Sathish",
+ 		"Status": true
+    },
+	{
+        "Course": "SpringBoot",
+        "TutorName": "Arun",
+ 		"Status": true
+    },
+	{
+         "Course": "Mule",
+        "TutorName": "Basav",
+ 		"Status": true
+    },
+	{
+       "Course": "Jmeter",
+        "TutorName": "Dhamu",
+ 		"Status": true
+    },
+	{
+        "Course": "JSP",
+        "TutorName": "Manish",
+ 		"Status": true
+    }]`)
+	return exampleData
+}
+
+
 
 //GetStory is used for get story service
-func (r ReadMyStoryServiceServer) GetStory(ctx context.Context, req *streamexp.RequestReadStory) (*streamexp.ReadStoryVO, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method GetStory not implemented")
+func (r ReadServer) GetStory(ctx context.Context, req *streamexp.RequestReadStory) (*streamexp.ReadStoryVO, error) {
+	log.Println("Getting inside.................",req.GetCourse())
+	switch req.GetCourse() {
+	case "GO":
+		aStudent := &streamexp.ReadStoryVO{
+			Course:   "GO",
+			TutorName: "Sathish",
+			Status: true,
+		}
+		return aStudent, nil
+	case "Java":
+		aStudent := &streamexp.ReadStoryVO{
+			Course:   "Java",
+			TutorName: "BALU",
+			Status: true,
+		}
+		return aStudent, nil
+	default :
+		aStudent := &streamexp.ReadStoryVO{
+			Course:   "HTML",
+			TutorName: "BABU",
+			Status: true,
+		}
+		return aStudent, nil
+	}
 }
 
-/*func (r ReadMyStoryServiceServer) ListOfStory(req *streamexp.RequestReadStory, srv streamexp.ReadMyStoryService_ListOfStoryServer) error {
-	return status.Errorf(codes.Unimplemented, "method ListOfStory not implemented")
+func (r ReadServer) ListOfStory(req *streamexp.RequestReadStory, srv streamexp.ReadMyStoryService_ListOfStoryServer) error {
+	log.Println("Getting inside of ListOfStory.................",req.GetCourse())
+	var savedFeatures []*streamexp.ReadStoryVO
+	var data []byte
+		data = generateStub()
+
+	if err := json.Unmarshal(data, &savedFeatures); err != nil {
+		log.Fatalf("Failed to load default Course Data: %v", err)
+	}
+	for _, course := range savedFeatures {
+			if err := srv.Send(course); err != nil {
+				return err
+			}
+	}
+	return nil
+
+
+
+
 }
-func (r ReadMyStoryServiceServer) StoryStatus(srv streamexp.ReadMyStoryService_StoryStatusServer) error {
-	return status.Errorf(codes.Unimplemented, "method StoryStatus not implemented")
+
+func (r ReadServer) StoryStatus(srv streamexp.ReadMyStoryService_StoryStatusServer) error {
+	log.Println("Getting inside of StoryStatus.................")
+	var savedFeatures []*streamexp.ReadStoryVO
+	var data []byte
+	data = generateStub()
+
+	if err := json.Unmarshal(data, &savedFeatures); err != nil {
+		log.Fatalf("Failed to load default Course Data: %v", err)
+	}
+	/*var (
+			crsstatus bool
+			tutorName string
+		)*/
+
+
+	for{
+
+		courseObj, err := srv.Recv()
+		if courseObj.Course != "" {
+			/*for _, course := range savedFeatures {
+				if course.Course == courseObj.Course {
+					fmt.Printf("data.................%v %v ", course.Status, course.TutorName)
+						crsstatus = course.Status
+						tutorName = course.TutorName
+
+				}
+			}*/
+
+			if err == io.EOF {
+				log.Println("courseObj.Course................", courseObj.Course)
+				return srv.SendAndClose(&streamexp.ReadStoryVO{Course: courseObj.Course, TutorName: "Test", Status: false,})
+			}
+
+			if err != nil {
+				log.Fatalf("Failed reciving data %v", err)
+				return err
+			}
+		}
+	}
+	return nil
 }
-func (r ReadMyStoryServiceServer) FullStory(srv streamexp.ReadMyStoryService_FullStoryServer) error {
+
+func (r ReadServer) FullStory(srv streamexp.ReadMyStoryService_FullStoryServer) error {
 	return status.Errorf(codes.Unimplemented, "method FullStory not implemented")
 }
-*/
+
 func main() {
-
-	var obj ReadMyStoryServiceServer
-
+	var obj ReadServer
 	srv := grpc.NewServer()
-
 	streamexp.RegisterReadMyStoryServiceServer(srv, obj)
 	lis, err := net.Listen("tcp", ":8888")
 	if err != nil {
 		log.Fatalf("Could not listen to :8888 :%v", err)
 	}
 	log.Fatal(srv.Serve(lis))
-
 }
